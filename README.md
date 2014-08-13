51d85e451d46d9fb1b25ef65c1914f30ad95daf5
Stefan Scherer <scherer_stefan@icloud.com>
2014-08-14 00:37:00 +0200

    added some doc to README, fix install dependencies

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-iruby-box
-=========
+# IRuby box
 
-IRuby in an Ubuntu Desktop box
+With this Vagrantfile you can install IRuby in an Ubuntu Desktop box.
+
+## Installation
+
+```
+git clone https://github.com/StefanScherer/iruby-box
+cd iruby-box
+vagrant up
+```
+
+## Create new IRuby Notebok
+```
+cd
+mkdir /p demo
+cd demo
+iruby notebook
+```
+
+Open up your browser inside the VM and open `http://anonymous:8888`
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,7 +1,22 @@
-sudo apt-get install -qq python-pip
-sudo apt-get install -qq python-dev
-sudo apt-get install -qq ruby
-sudo apt-get install -qq ruby-dev
-sudo apt-get install -qq libzmq-dev
+sudo apt-get update -y
+sudo apt-get install -y python-pip
+sudo apt-get install -y python-dev
+sudo apt-get install -y ruby
+sudo apt-get install -y ruby-dev
+sudo apt-get install -y libzmq3-dev
+sudo apt-get install libffi-dev
+
+sudo gem install pry
+sudo gem install awesome_print
 sudo pip install ipython[all] --upgrade
 sudo gem install iruby
+
+
+#==> default:   * pry-doc
+#==> default:   * pry-theme
+#==> default:   * pry-syntax-hacks
+#==> default:   * pry-git
+#==> default:   * gruff
+#==> default:   * rmagick
+#==> default:   * gnuplot
+#==> default:   * rubyvis
